14a833428555f5f23882358de32d41d25b360aa5
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-17 07:39:25 +0800

    Declared the variables needed for the functionality of the scoreboard

---
+++ index.js
@@ -0,0 +1,11 @@
+let scoreContent = document.getElementById("score");
+let plusOneHomeButton = document.getElementById("plus-one-home");
+let plusTwoHomeButton = document.getElementById("plus-two-home");
+let plusThreeHomeButton = document.getElementById("plus-three-home");
+let plusOneGuestButton = document.getElementById("plus-one-guest");
+let plusTwoGuestutton = document.getElementById("plus-two-home-guest");
+let plusThreeGuestButton = document.getElementById("plus-three-home-guest");
+let score = 0;
+
+
+
