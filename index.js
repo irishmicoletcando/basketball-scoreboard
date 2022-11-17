730f4b6b93ecfd31b614093b7fc1493238cb92f2
Irish Micole Cando <irishmicolecando@gmail.com>
2022-11-17 14:58:19 +0800

    Removed event listeners since they caused scores to doubled and fixed the plusOne function

--- index.js
+++ index.js
@@ -10,42 +10,32 @@ const plusTwoHomeButton = document.getElementById("plus-two-home");
 const plusThreeHomeButton = document.getElementById("plus-three-home");
 // guest buttons
 const plusOneGuestButton = document.getElementById("plus-one-guest");
-const plusTwoGuestutton = document.getElementById("plus-two-guest");
+const plusTwoGuestButton = document.getElementById("plus-two-guest");
 const plusThreeGuestButton = document.getElementById("plus-three-guest");
 
 // adding scores based on button clicked
 function plusOneHome() {
-  scoreHomeContent.textContent = scoreHome++;
-}
+  scoreHomeContent.textContent = scoreHome += 1;
+};
 
 function plusTwoHome() {
   scoreHomeContent.textContent = scoreHome += 2;
-}
+};
 
 function plusThreeHome() {
   scoreHomeContent.textContent = scoreHome += 3;
-}
+};
 
 function plusOneGuest() {
-  scoreGuestContent.textContent = scoreGuest++;
-}
+  scoreGuestContent.textContent = scoreGuest += 1;
+};
 
 function plusTwoGuest() {
   scoreGuestContent.textContent = scoreGuest += 2;
-}
+};
 
 function plusThreeGuest() {
   scoreGuestContent.textContent = scoreGuest += 3;
-}
-
-// for home div
-plusOneHomeButton.addEventListener("click", plusOneHome);
-plusTwoHomeButton.addEventListener("click", plusTwoHome);
-plusThreeHomeButton.addEventListener("click", plusThreeHome);
-
-// for guest div
-plusOneGuestButton.addEventListener("click", plusOneGuest);
-plusTwoGuestButton.addEventListener("click", plusTwoGuest);
-plusThreeGuestButton.addEventListener("click", plusThreeGuest);
+};
 
 
